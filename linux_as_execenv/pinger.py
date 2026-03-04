a8bf0c1b729f4e3b88027a5e2a6b5ed60f799b26
Sanya <sasabuzinaev@gmail.com>
2026-03-04 20:39:47 +0800

    deploy pinger data date report

---
+++ linux_as_execenv/pinger.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime, timedelta
+
+url_template = "https://simurg.space/gen_file?data=obs&date={date}"
+
+current = datetime.now()
+while True:
+    current = current - timedelta(days=1) 
+    date_str = current.strftime("%Y-%m-%d")
+    print(f"Current datetime is {current} and {date_str}")
+
+    url = url_template.format(date=date_str)
+
+    response = requests.get(url=url, stream=True)
+    print(f"From url {url} got {response}")
+    if response.status_code == 200:
+        print(f"Last avalibe data are for {current}")
+        break
+
+
