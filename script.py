0d9beab7be23b7ceed4d498597df177e07b7665b
Sanya <sasabuzinaev@gmail.com>
2026-03-04 19:33:44 +0800

    Deploy function script

---
+++ script.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+def add2(a,b):
+    return a*b
+def add3(a,b,c):
+    return a+b+c
+def add4(a,b,c,d):
+    return a+b+c+d
+def add5(a,b,c,d):
+    return a*b*c+d
+print(add(5, 3))
+print(add(2,5))
+print(add2(3,4))
+print(add3(3,4))
+print(add4(3,4,5))
+print(add5(3,4,5,6))
