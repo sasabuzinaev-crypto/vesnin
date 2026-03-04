637192c80d46fd16962ef0b6591d83e9b70e180f
Sanya <sasabuzinaev@gmail.com>
2026-03-04 21:23:06 +0800

    showed avoid-duplicate-logic pr

--- linux_as_execenv/testing/tests/test-.py
+++ linux_as_execenv/testing/tests/test-.py
@@ -16,7 +16,15 @@ def test_bug_addition_enough():
     assert add_with_bug(0,0) == 0, "Function didn't returned 0"
     #assert add_with_bug(5,6) == 11
     print("Test BUG ADDITION PASSED")
+def test_addition_dublicated_logic():
+    #bad test since it relies on absense of "+" in add()
+    assert add(6,3) == 6+3
+    print("Test DUBLICATED LOGIC PASSED (could get some bugs in)")
+    #good test since input and output are independent in add()
+    assert add(6,3) == 9
+    print("Test DUBLICATED LOGIC PASSED")
 if __name__ == '__main__':
     test_addition_basic()
     test_bug_addition_notsufficent()
     test_bug_addition_enough()
+    test_addition_dublicated_logic()
