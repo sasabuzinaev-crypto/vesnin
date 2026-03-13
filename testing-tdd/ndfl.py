8cfae477ac6662011106f83d052240a531ba739d
Sasha <sashabuzinaevicloud.com>
2026-03-14 00:26:39 +0800

    added ndfl test with big numbers

---
+++ testing-tdd/ndfl.py
@@ -0,0 +1,7 @@
+def calculate_tax(income):
+    if income<2400000:
+        return income *0.13
+    elif income <5_000_000:
+        return(2_400_00*0.13 +(income -2_400_00)*0.15)
+    elif income <20_000_000:
+        return(2_400_000 *0.13 +2_600_00*0.15+(income-5_000_000)*0.15)
