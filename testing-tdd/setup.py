1b32671815ce5aafe9511daa425cd341554f1a6b
Sasha <sasabuzinaev@gmail.com>
2026-03-31 00:02:32 +0800

    feat: added dependecies

--- testing-tdd/setup.py
+++ testing-tdd/setup.py
@@ -4,5 +4,7 @@ setup(
     name="ndfl",
     version="0.0.0",
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
+    description="README.md",
+    url="https://github.com/sasabuzinaev-crypto/vesnin/tree/feature/testing-tdd"
 )
