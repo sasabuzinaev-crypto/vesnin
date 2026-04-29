b8a8c07fb7cbb9b552174210f4a83ae42babf2c1
Sanya <sasabuzinaev@gmail.com>
2026-04-29 14:06:46 +0800

    add

--- testing-tdd/setup.py
+++ testing-tdd/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ndfl",
+    name="ndflybu",
     version="0.0.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
